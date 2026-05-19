b5198072da0be92369ff1e0e2feb6cafe326c62f
pranavmali-create <pranavmali0107@gmail.com>
2026-05-19 15:54:21 +0530

    refactor(kanata): use config vars for urls

--- scripts/kanata.ps1
+++ scripts/kanata.ps1
@@ -1,3 +1,6 @@
+$kanata_URL = https://github.com/jtroo/kanata/releases/download/v1.11.0/windows-binaries-x64.zip 
+$my_config = https://raw.githubusercontent.com/pranavmali-create/kanata-config/refs/heads/main/kanata.kbd
+
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
 if (-not $currentPrincipal.IsInRole($adminRole)) {
@@ -11,10 +14,10 @@ if(Get-Command "kanata.exe" -ErrorAction SilentlyContinue) {
     } 
 else{
     mkdir "C:\tools\kanata"
-    Invoke-WebRequest -Uri "https://github.com/jtroo/kanata/releases/download/v1.11.0/windows-binaries-x64.zip" -OutFile "C:\tools\kanata\kanata.zip"
+    Invoke-WebRequest -Uri '$kanata-config' -OutFile "C:\tools\kanata\kanata.zip"
     Expand-Archive -Path "C:\tools\kanata\kanata.zip" -DestinationPath "C:\tools\kanata" 
 }
 
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pranavmali-create/kanata-config/refs/heads/main/kanata.kbd" -OutFile "C:\tools\kanata\kanata.kbd"
+    Invoke-WebRequest -Uri '$my_config' -OutFile "C:\tools\kanata\kanata.kbd"
 
     & "C:\tools\kanata\kanata_windows_tty_winIOv2_x64.exe" --cfg "C:\tools\kanata\kanata.kbd"
